5064eb03b9494e5ce8dcf76ad795a8d2903c3ea1
Sepehr Laal <sepehr.laal@gmail.com>
2018-08-09 18:03:43 -0700

    added curl to container for healthcheck + cleanup after installs

--- build.sh
+++ build.sh
@@ -3,9 +3,13 @@ set -e
 set -x
 
 apt-get update -qq
-apt-get install -y --no-install-recommends dumb-init stunnel4 squid
+apt-get install -y --no-install-recommends dumb-init stunnel4 squid curl
 
 # generate certifacte used stunnel (can override from outside with mounts)
 openssl genrsa -out key.pem 2048
 openssl req -new -x509 -key key.pem -out cert.pem -days 1095 -subj '/CN=squid-proxy/O=NULL/C=AU'
 cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+
+rm -rf /var/lib/apt/lists/* /usr/share/doc/ /usr/share/man/ /usr/share/locale/
+apt-get autoremove -y
+apt-get clean -y
